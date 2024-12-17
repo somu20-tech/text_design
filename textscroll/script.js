0470e327dfb96e61091b70620b5aa882a130d93a
Subhranshu_Maharana <maharanasomu20@gmail.com>
2024-12-17 20:03:05 +0530

    Add files via upload

---
+++ textscroll/script.js
@@ -0,0 +1,36 @@
+window.addEventListener("wheel",function(dets){
+    if(dets.deltaY>0){
+        gsap.to(".marque",{
+            transform:"translateX(-200%)",
+            repeat:-1,
+            duration:4,
+            ease:"none"
+        })
+
+        gsap.to(".marque img",{
+            rotate:190
+        })
+
+        gsap.to("#move",{
+            backgroundColor:"navy",
+            color:"aqua"
+        })
+
+    }
+    else{
+        gsap.to(".marque",{
+            transform:"translateX(0%)",
+            repeat:-1,
+            duration:4,
+            ease:"none"
+        })
+        gsap.to(".marque img",{
+            rotate:-30
+        })
+
+        gsap.to("#move",{
+            backgroundColor:"green",
+            
+        })
+    }
+})
